8abcc642259a9c530456e03fab7ca81558b5cace
PableteProgramming <pabletegym@icloud.com>
2020-10-04 20:40:48 +0200

    Paginas detectadas con coma y guion pero error al juntar intervalo

--- PdfExtractor/Form1.Designer.cs
+++ PdfExtractor/Form1.Designer.cs
@@ -29,23 +29,152 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.InputButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.OutputButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Extract = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pdf page extractor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input File:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TabStop = false;
+            // 
+            // InputButton
+            // 
+            this.InputButton.Location = new System.Drawing.Point(415, 46);
+            this.InputButton.Name = "InputButton";
+            this.InputButton.Size = new System.Drawing.Size(75, 23);
+            this.InputButton.TabIndex = 3;
+            this.InputButton.TabStop = false;
+            this.InputButton.Text = "browse";
+            this.InputButton.UseVisualStyleBackColor = true;
+            this.InputButton.Click += new System.EventHandler(this.InputButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output file:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TabStop = false;
+            // 
+            // OutputButton
+            // 
+            this.OutputButton.Location = new System.Drawing.Point(416, 96);
+            this.OutputButton.Name = "OutputButton";
+            this.OutputButton.Size = new System.Drawing.Size(75, 23);
+            this.OutputButton.TabIndex = 6;
+            this.OutputButton.TabStop = false;
+            this.OutputButton.Text = "browse";
+            this.OutputButton.UseVisualStyleBackColor = true;
+            this.OutputButton.Click += new System.EventHandler(this.OutputButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Pages:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(78, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(412, 23);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TabStop = false;
+            // 
+            // Extract
+            // 
+            this.Extract.BackColor = System.Drawing.Color.Lime;
+            this.Extract.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Extract.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Extract.Location = new System.Drawing.Point(14, 172);
+            this.Extract.Name = "Extract";
+            this.Extract.Size = new System.Drawing.Size(476, 52);
+            this.Extract.TabIndex = 9;
+            this.Extract.TabStop = false;
+            this.Extract.Text = "Extract";
+            this.Extract.UseVisualStyleBackColor = false;
+            this.Extract.Click += new System.EventHandler(this.Extract_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 227);
+            this.ClientSize = new System.Drawing.Size(502, 232);
+            this.Controls.Add(this.Extract);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.OutputButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.InputButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button InputButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button OutputButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Extract;
     }
 }
 
--- PdfExtractor/Form1.cs
+++ PdfExtractor/Form1.cs
@@ -12,10 +12,147 @@ namespace PdfExtractor
 {
     public partial class Form1 : Form
     {
+        string inputfile;
+        string outputfile;
+        string pagesstring;
+        int[] pages;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void InputButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = ".";
+            dialog.Multiselect = false;
+            dialog.Filter = "pdf files (*.pdf)|*.pdf";
+            if(dialog.ShowDialog()== DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+                inputfile = textBox1.Text;
+            }
+        }
+
+        private void OutputButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "pdf files (*.pdf)|*.pdf";
+            dialog.InitialDirectory = ".";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = dialog.FileName;
+                outputfile = textBox2.Text;
+            }
+        }
+        private bool AllIntegers(string page, char sep1, char sep2)
+        {
+            for (int i = 0; i < page.Length; i++)
+            {
+                char c = page[i];
+                if(c!=sep1 && c != sep2)
+                {
+                    try
+                    {
+                        int a= (int)Char.GetNumericValue(c);
+                    }
+                    catch(Exception ex)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private int[] GetAllNumbersBetween(int n, int n1)
+        {
+            List<int> r = new List<int>(); 
+            for(int i=n; i<=n1; i++)
+            {
+                r.Add(i);
+            }
+            int[] ret = new int[r.Count];
+            for(int i=0; i < ret.Length; i++)
+            {
+                ret[i] = r[i];
+            }
+            return ret;
+        }
+
+
+        private int[] GetPagesFromString(string page, char sep1, char sep2)
+        {
+            List<int> r = new List<int>();
+            string s = "";
+            for (int i = 0; i < page.Length; i++)
+            {
+                char c = page[i];
+                if(c!=sep1 && c!= sep2)
+                {
+                    s += c;
+                }
+                else
+                {
+                    if (c == sep1)//,
+                    {
+                        r.Add(int.Parse(s));
+                        s = "";
+                    }
+                    else if (c == sep2)//-
+                    {
+                        string ss = "";
+                        int num1= int.Parse(s);
+                        int num2;
+                        for(int j=i+1; j < page.Length; j++)
+                        {
+                            char cc = page[j];
+                            if(cc!=sep1 && cc!= sep2)
+                            {
+                                ss += cc;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        num2 = int.Parse(ss);
+                        int[] nums = GetAllNumbersBetween(num1, num2);
+                        r.AddRange(nums);
+                    }
+                }
+            }
+            r.Add(int.Parse(s));
+            int[] ret = new int[r.Count];
+            for(int i=0; i < ret.Length; i++)
+            {
+                ret[i] = r[i];
+            }
+            return ret;
+        }
+
+        private void Extract_Click(object sender, EventArgs e)
+        {
+            outputfile = textBox2.Text;
+            inputfile = textBox1.Text;
+            pagesstring = textBox3.Text;
+            if(outputfile.Trim()!="" && inputfile.Trim() != "" && pagesstring.Trim()!="")
+            {
+                if (AllIntegers(pagesstring, ',', '-'))
+                {
+                    pages = GetPagesFromString(pagesstring, ',', '-');
+                    string ss = "";
+                    foreach(int i in pages)
+                    {
+                        ss += i.ToString()+";";
+                    }
+                    MessageBox.Show(ss, "Pages", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill the pages and the input and output file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
